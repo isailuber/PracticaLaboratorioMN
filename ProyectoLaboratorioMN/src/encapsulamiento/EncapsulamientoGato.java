4d72b660bf5c370d195c5774aafe449d37973e1b
HOME <HOME@HOME-PC>
2021-02-27 14:14:20 -0500

    Se agregaron los atributos y la función de mostrar en pantalla

--- ProyectoLaboratorioMN/src/encapsulamiento/EncapsulamientoGato.java
+++ ProyectoLaboratorioMN/src/encapsulamiento/EncapsulamientoGato.java
@@ -1,10 +1,23 @@
 package encapsulamiento;
 
+
 public class EncapsulamientoGato {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public String nombre;
+	public int patas;
 
+	public EncapsulamientoGato() {
+
+		this.nombre = null;
+		this.patas = 0;
 	}
 
+	public static void main(String[] args) {
+		EncapsulamientoGato g = new EncapsulamientoGato();
+		g.nombre = "Tomi";
+		g.patas = 4;
+		System.out.println("El gato se llama: " + g.nombre);
+		System.out.println("El numero de patas de mi gato es:" + g.patas);
+
+	}
 }
