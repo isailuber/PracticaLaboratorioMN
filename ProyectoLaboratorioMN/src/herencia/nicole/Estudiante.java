3bd1ba67101436609be4cfa6d0817800a4c13311
isailuber <79735790+isailuber@users.noreply.github.com>
2021-02-27 14:47:39 -0500

    Se agregaron Atributos Metos, Constructores
    Realizado por Nicole

--- ProyectoLaboratorioMN/src/herencia/nicole/Estudiante.java
+++ ProyectoLaboratorioMN/src/herencia/nicole/Estudiante.java
@@ -1,5 +1,42 @@
 package herencia.nicole;
 
 public class Estudiante {
+  //Atributos
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	//Constructor
+	public Persona(String nombre, String apellido, int edad) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	//Metodos getters
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	//Metodos setters
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 
+	
 }
+
