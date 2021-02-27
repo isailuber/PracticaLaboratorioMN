1dbef7f6f832e37df57ea431918bb22ed0470639
HOME <HOME@HOME-PC>
2021-02-27 14:24:31 -0500

    Se heredó de la clase abstracta y se construyó el método resta

--- ProyectoLaboratorioMN/src/abstraccion/OpercionesEntreNumeros.java
+++ ProyectoLaboratorioMN/src/abstraccion/OpercionesEntreNumeros.java
@@ -1,5 +1,16 @@
 package abstraccion;
 
-public class OpercionesEntreNumeros {
+public class OpercionesEntreNumeros extends SumaRestaEntreNumeros {
+	@Override
+	public int restar(int argumento1, int argumento2) {
+		return argumento1 - argumento2;
+	}
 
+	// llama al metodo protected de la clase publica.
+	public int sumar(int argumento1, int argumento2) {
+
+		// para llamar al metodo de la clase padre
+		// usamos la palabra clave "super".
+		return super.sumar(argumento1, argumento2);
+	}
 }
