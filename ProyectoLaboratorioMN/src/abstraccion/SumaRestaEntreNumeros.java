9f9e10078042ebe1126e40cd3ae29faf0d59e3f0
HOME <HOME@HOME-PC>
2021-02-27 14:21:42 -0500

    Se dio atributos a la clase abstracta y agregamos dos métodos suma,resta

--- ProyectoLaboratorioMN/src/abstraccion/SumaRestaEntreNumeros.java
+++ ProyectoLaboratorioMN/src/abstraccion/SumaRestaEntreNumeros.java
@@ -1,5 +1,22 @@
 package abstraccion;
 
-public class SumaRestaEntreNumeros {
+public abstract class SumaRestaEntreNumeros {
+	
+	// Atributo
+		private int propiedad;
+		public static final int CONSTANTE = 1;
+		
+		public SumaRestaEntreNumeros() {
+
+			this.propiedad = 0;
+		}
+
+		// un metodo normal como en cualquier clase
+		protected int sumar(int argumento1, int argumento2) {
+			return argumento1 + argumento2;
+		}
+
+		// clase abstracta.
+		public abstract int restar(int argumento1, int argumento2);
 
 }
