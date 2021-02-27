71f43dd6e158f3f426df0ae5a570d9b7c573a6bb
HOME <HOME@HOME-PC>
2021-02-27 14:26:48 -0500

    se instanciaron los objetos y se agregaron a la función ejecutable

--- ProyectoLaboratorioMN/src/abstraccion/Principal.java
+++ ProyectoLaboratorioMN/src/abstraccion/Principal.java
@@ -3,7 +3,17 @@ package abstraccion;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Esta variable solo puede invocar metodos de la clase SumaRestaEntreNumeros.
+		// No puede invocar metodos de la clase OperacionesEntreNumeros.
+		SumaRestaEntreNumeros variable = new OpercionesEntreNumeros();
+
+		// esta variable si puede invocar metodos de la clase OperacionesEntreNumeros
+		// que sean publicos. Caso contrario, no puede.
+		OpercionesEntreNumeros variable2 = new OpercionesEntreNumeros();
+
+		System.out.println("invocando restar: " + variable.restar(5, 2));
+
+		System.out.println("invocado sumar: " + variable2.sumar(5, 2));
 
 	}
 
